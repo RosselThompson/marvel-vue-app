b3b7dacfb65b2b8fd13d987e89ea5cfd9546a185
Rossel Thompson <rossel96t@gmail.com>
2022-09-17 00:02:45 -0600

    feat: use alias on storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,18 @@
+const path = require("path");
+
 module.exports = {
   stories: [
     '../src/components/**/*.stories.mdx',
     '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src"),
+    };
+    config.resolve.extensions.push(".ts");
+    return config;
+  },
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
