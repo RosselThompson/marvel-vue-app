c812ca5ffd19817b5daa09848ce82a36ac841070
Rossel Thompson <rossel96t@gmail.com>
2022-09-17 13:29:26 -0600

    fix: refactor style

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="h-screen flex flex-col">
         <navbar :menuOptions="menuOptions" />
-        <div style="z-index: -9999;" class="absolute top-16 p-4 w-full h-full lg:p-0 lg:justify-around lg:flex">
+        <div class="z-overlap absolute top-16 p-4 w-full h-full lg:p-0 lg:justify-around lg:flex">
             <div class="d-none lg:flex"></div>
             <div class="lg:flex lg:justify-center lg:w-3/4 xl:w-1/2">
                 <slot></slot>
@@ -28,27 +28,7 @@ export default defineComponent({
 </script>
 
 <style>
-.layout {
-    display: flex;
-    flex-direction: row;
-    height: 100vh;
-    width: 100%;
-}
-
-.layout-menu {
-    display: flex;
-    width: 200px;
-    background-color: #eee;
-    border-right: 1px solid #ddd;
-}
-
-.layout-menu-list {
-    display: flex;
-    flex-direction: column;
-}
-
-.layout-content {
-    display: flex;
-    flex-grow: 1;
+.z-overlap {
+    z-index: -9999;
 }
 </style>
